be4ba207291a3cde1a42e2f1e0880b96d4a14e6f
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-09-22 17:22:37 +0300

    1 Draft functions of identify atom numbers by input formula.

--- HW12_Cpp_OOP.cpp
+++ HW12_Cpp_OOP.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 
 using std::cout;
@@ -6,16 +7,83 @@ char arrAtomName[][3][20]{ {"H", "Hydrogen"}, {"He", "Helium"}, {"Li", "Lithium"
     {"Na", "Sodium"}, {"Mg", "Magnesium"}, {"Al", "Aluminum"}, {"Si", "Silicon"}, {"P", "Phosphorus"},
     {"S", "Sulfur"}, {"Cl", "Chlorine"}, {"Ar", "Argon"} };
 
+int AtomNumber(char s) {
 
+    int size = sizeof(arrAtomName) / sizeof(arrAtomName[0]);
+
+    for (int i = 0; i < size; i++)
+    {
+        if (sizeof(arrAtomName[i][0]) / sizeof(arrAtomName[i][0][0]) != 2)
+            if (s == arrAtomName[i][0][0])
+                return ++i;
+    }
+}
+;
 int main(void)
 {
-       
     char str[] = "C2H5OH";
     char* ps = str;
 
     int size = sizeof(str) / sizeof(str[0]);
     int count(0);
     bool isSymbolRepeat;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (static_cast<int>(str[i]) > 64 && static_cast<int>(str[i]) < 91)
+        {
+            isSymbolRepeat = false;
+
+            for (int j = 0; j < i; j++)
+            {
+                if (str[i] == str[j]) {
+                    isSymbolRepeat = true;
+                    break;
+                }
+            }
+
+            if (!isSymbolRepeat)
+                count++;
+        }
+    }
+
+    int** arrAtomNumber = new int* [count];
+    for (int i = 0; i < count; i++)
+        arrAtomNumber[i] = new int[2];
+
+    int ind = 0;
+    for (int i = 0; i < size; i++)
+    {
+        isSymbolRepeat = false;
+
+        if (static_cast<int>(str[i]) > 64 && static_cast<int>(str[i]) < 91)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (str[i] == str[j]) {
+                    isSymbolRepeat = true;
+                    break;
+                }
+            }
+
+            if (!isSymbolRepeat) {
+                arrAtomNumber[ind][0] = AtomNumber(str[i]);
+                ind++;
+            }
+        }
+    }
+
+    for (int i = 0; i < count; i++)
+        cout << arrAtomNumber[i][0] << "\t";
+
+    for (int i = 0; i < count; i++) {
+        delete[] arrAtomNumber[i];
+        arrAtomNumber[i] = nullptr;
+    }
+
+    delete[] arrAtomNumber;
+    arrAtomNumber = nullptr;
+    ps = nullptr;
 
     return 0;
 }
