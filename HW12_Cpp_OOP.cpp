7011e1e2859bfddbd0a051076c5b1ef07deb98ad
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-09-22 19:26:40 +0300

    2 Drafted function for identify the summary quantity of atoms by each unique chemic elements.

--- HW12_Cpp_OOP.cpp
+++ HW12_Cpp_OOP.cpp
@@ -7,30 +7,59 @@ char arrAtomName[][3][20]{ {"H", "Hydrogen"}, {"He", "Helium"}, {"Li", "Lithium"
     {"Na", "Sodium"}, {"Mg", "Magnesium"}, {"Al", "Aluminum"}, {"Si", "Silicon"}, {"P", "Phosphorus"},
     {"S", "Sulfur"}, {"Cl", "Chlorine"}, {"Ar", "Argon"} };
 
-int AtomNumber(char s) {
+int fillAtomNumber(char s) {
 
     int size = sizeof(arrAtomName) / sizeof(arrAtomName[0]);
 
     for (int i = 0; i < size; i++)
     {
-        if (sizeof(arrAtomName[i][0]) / sizeof(arrAtomName[i][0][0]) != 2)
+        if (arrAtomName[i][0][1] == '\0')
             if (s == arrAtomName[i][0][0])
                 return ++i;
     }
 }
 ;
+void fillAtomQuntity(char *arrElement, int ** arrAtomNumber, char s, int value) {
+
+    int indElement = 0;
+    for (int i = 0; i < sizeof(arrElement) / sizeof(arrElement[0]); i++)
+    {
+        if (s == arrElement[i]) {
+            indElement = i;
+            break;
+        }
+    }
+
+    arrAtomNumber[indElement][1] += value;
+}
+;
+void fillAtomQuntity(char *arrElement, int ** arrAtomNumber, char s) {
+
+    int indElement = 0;
+    for (int i = 0; i < sizeof(arrElement) / sizeof(arrElement[0]); i++)
+    {
+        if (s == arrElement[i]) {
+            indElement = i;
+            break;
+        }
+    }
+
+    arrAtomNumber[indElement][1] += 1;
+}
+;
 int main(void)
 {
-    char str[] = "C2H5OH";
+    char str[] = "CO2H2SO4";
     char* ps = str;
 
-    int size = sizeof(str) / sizeof(str[0]);
+    int size = (sizeof(str) / sizeof(str[0])) - 1;
     int count(0);
     bool isSymbolRepeat;
 
+    //function for indentify the quantity of unique chemic elements
     for (int i = 0; i < size; i++)
     {
-        if (static_cast<int>(str[i]) > 64 && static_cast<int>(str[i]) < 91)
+        if (static_cast<int>(str[i]) >= 65 && static_cast<int>(str[i]) <= 90)
         {
             isSymbolRepeat = false;
 
@@ -47,16 +76,20 @@ int main(void)
         }
     }
 
+    //function create memory
+    char* arrElements = new char[count];
+
     int** arrAtomNumber = new int* [count];
     for (int i = 0; i < count; i++)
-        arrAtomNumber[i] = new int[2];
+        arrAtomNumber[i] = new int[2] {0, 0};
 
+    //function for identify atomic number of unique chemic elements
     int ind = 0;
     for (int i = 0; i < size; i++)
     {
         isSymbolRepeat = false;
 
-        if (static_cast<int>(str[i]) > 64 && static_cast<int>(str[i]) < 91)
+        if (static_cast<int>(str[i]) >= 65 && static_cast<int>(str[i]) <= 90)
         {
             for (int j = 0; j < i; j++)
             {
@@ -67,15 +100,39 @@ int main(void)
             }
 
             if (!isSymbolRepeat) {
-                arrAtomNumber[ind][0] = AtomNumber(str[i]);
+                arrElements[ind] = str[i];
+                arrAtomNumber[ind][0] = fillAtomNumber(str[i]);
                 ind++;
             }
         }
     }
 
-    for (int i = 0; i < count; i++)
-        cout << arrAtomNumber[i][0] << "\t";
+    //function for identify the summary quantity of atoms by each unique chemic elements
+    for (int i = 0; i < size; i++)
+    {
+        if (i == size - 1) {
+            if (!(static_cast<int>(str[i]) >= 48 && static_cast<int>(str[i]) <= 57))
+                fillAtomQuntity(arrElements, arrAtomNumber, str[i]);
+            break;
+        }
+        else if (static_cast<int>(str[i]) >= 48 && static_cast<int>(str[i]) <= 57)
+            continue;
+        else if (static_cast<int>(str[i + 1]) >= 48 && static_cast<int>(str[i + 1]) <= 57)
+        {
+            int value = static_cast<int>(str[i + 1]) % 48;
+            fillAtomQuntity(arrElements, arrAtomNumber, str[i], value);
+        }
+        else if (!(static_cast<int>(str[i + 1]) >= 48 && static_cast<int>(str[i + 1]) <= 57))
+            fillAtomQuntity(arrElements, arrAtomNumber, str[i]);
+    }
 
+    //function show result numberAtom : quantityAtom
+    for (int i = 0; i < count; i++) 
+    {
+        cout << arrAtomNumber[i][0] << "\t" << arrAtomNumber[i][1] << "\n";
+    }
+
+    //function clear memory
     for (int i = 0; i < count; i++) {
         delete[] arrAtomNumber[i];
         arrAtomNumber[i] = nullptr;
@@ -83,6 +140,10 @@ int main(void)
 
     delete[] arrAtomNumber;
     arrAtomNumber = nullptr;
+
+    delete[] arrElements;
+    arrElements = nullptr;
+
     ps = nullptr;
 
     return 0;
